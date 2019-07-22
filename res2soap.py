ed699a64a690cfe5176ae0611e17ef605dbc6d65
Bonan Zhu <zhubonan@outlook.com>
2019-07-22 16:17:43 +0100

    Use click for commandline interface

--- res2soap.py
+++ res2soap.py
@@ -3,31 +3,49 @@
 from ase.io import read
 import quippy
 from tqdm import tqdm
-import numpy as np
-import glob
-import sys
 import os
 from multiprocessing import Pool
 from subprocess import check_output
+import click
 
-try:
-    WORKDIR = sys.argv[1]
-except KeyError:
-    WORKDIR = './'
-
-desc = quippy.descriptors.Descriptor(
-    "soap cutoff=5 l_max=15 n_max=15 atom_sigma=0.1 average=T n_species=1 species_Z=13 Z=13"
-)
+DESC_INIT = False
 
 
-def get_soap_desc(args):
-    """Function returns the descript """
-    n, atoms = args
-    if not isinstance(atoms, quippy.Atoms):
-        atoms = quippy.Atoms(atoms)
-    atoms.set_cutoff(10)
-    atoms.calc_connect()
-    return n, desc.calc(atoms)['descriptor']
+class Res2SOAP(object):
+    def __init__(self, desc_dict):
+        """
+        """
+        self.desc_settings = desc_dict
+
+    @property
+    def desc_string(self):
+        settings = []
+        for key, value in self.desc_settings.items():
+            settings.append('{}={}'.format(key, value))
+        return 'soap ' + ' '.join(settings)
+
+    def init_desc(self):
+        """Initialize the descriptor object"""
+        self._desc = quippy.descriptors.Descriptor(self.desc_string)
+        # Set the module level parameter for parallelisation
+        global DESC_INIT
+        DESC_INIT = True
+
+    @property
+    def desc(self):
+        if not self._desc or DESC_INIT is False:
+            self.init_desc()
+        return self._desc
+
+    def get_soap_desc(self, args):
+
+        n, atoms = args
+        if not isinstance(atoms, quippy.Atoms):
+            atoms = quippy.Atoms(atoms)
+        cut_off = int(self.desc_settings['cutoff']) + 1
+        atoms.set_cutoff(cut_off)
+        atoms.calc_connect()
+        return n, self.desc.calc(atoms)['descriptor']
 
 
 def save_file(savename, desc_arrays, info):
@@ -40,22 +58,58 @@ def save_file(savename, desc_arrays, info):
             fh.write(line + '\n')
 
 
-if __name__ == '__main__':
-
-    # compute the descriptors in the same order as the cryan output
-    output = check_output(['ca', '-v'], cwd=WORKDIR).split(
+def get_res_paths(workdir=None):
+    if workdir is None:
+        workdir = './'
+    output = check_output(['ca', '-v'], cwd=workdir).split(
         '\n')  # Run ca -v in shell and get outputs
     num_struct, _ = output[0].split()
-    lines = output[int(num_struct) + 1:-1]
-    fnames = [os.path.join(WORKDIR, l.split('\t')[0] + '.res') for l in lines]
-    pool = Pool(4)
-    print('Reading res files')
+    airss_info = output[int(num_struct) + 1:-1]
+    fnames = [
+        os.path.join(workdir,
+                     l.split('\t')[0] + '.res') for l in airss_info
+    ]
+    return fnames, airss_info
+
+
+@click.command('res2soap')
+@click.argument('--workdir')
+@click.option('--nprocs', '-np', help='Number of processes for parallelisation.', defualt=4)
+@click.option('--save-name', '-s', help='Save file name', default='soap_desc.xyz')
+@click.option('--l-max', default=15)
+@click.option('--n-max', default=15)
+@click.option('--atoms-sigma', default=0.01)
+@click.option('--n-species', default=1)
+@click.option('--Z', '-z', required=True, type=int)
+@click.option('--species_Z', '-sz', required=True, type=int, mutiple=True)
+def main(cutoff, workdir, l_max, n_max, atoms_sigma, n_species, Z,
+         nprocs, save_name, species_Z):
+    """
+    Compute SOAP descriptors for res files, get the order or files from
+    the `ca -v` commands for consistency.
+    """
+    desc_settings = {
+        'cutoff': cutoff,
+        'l_max': l_max,
+        'n_max': n_max,
+        'atoms_sigma': atoms_sigma,
+        'average': 'T',
+        'n_species': n_species,
+        'Z': Z,
+        'species_Z': ','.join(species_Z)
+    }
+    comp = Res2SOAP(desc_settings)
+    fnames, airss_info = get_res_paths(workdir)
+    click.echo('Reading res files')
     atom_list = [[n, read(fn)] for n, fn in enumerate(tqdm(fnames))]
-    print('Computing descriptors')
-    par_res = list(pool.imap_unordered(get_soap_desc, tqdm(atom_list)))
+    pool = Pool(nprocs)
+    click.echo('Computing descriptors in {} way parallel'.format(nprocs))
+    par_res = list(pool.imap_unordered(comp.get_soap_desc, tqdm(atom_list)))
     par_res.sort(key=lambda x: x[0])
     _, descs = zip(*par_res)
+    click.echo('Saving files')
+    save_file(save_name, descs, airss_info)
 
-    # descs = [get_soap_desc(atoms) for atoms in tqdm(atom_list)]
-    print('Saving results')
-    save_file('soap_desc.xyz', descs, lines)
+
+if __name__ == '__main__':
+    main()
